1bebaf3894edd28967c656d396e35f9223ee8540
unknown <jani45@gmail.com>
2023-02-09 08:59:49 +0200

    dev datasource configs

---
+++ src/main/java/com/thesis/api/configs/DevDataSource.java
@@ -0,0 +1,22 @@
+package com.thesis.api.configs;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("dev")
+public class DevDataSource {
+    @Bean
+    public DataSource getDevDataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.driverClassName("org.postgresql.Driver");
+        builder.url("jdbc:postgresql://localhost:5432/apidb");
+        builder.username("postgres");
+        builder.password("postgres");
+        return builder.build();
+    }
+}
