7a27d28cb754e732a5f27e81e552fd3a6cdf82b0
unknown <jani45@gmail.com>
2023-02-10 11:34:16 +0200

    authorities for filterchain

--- src/main/java/com/thesis/api/configs/SecurityConfig.java
+++ src/main/java/com/thesis/api/configs/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,13 +16,33 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                // CORS Enabled
                 .cors().and()
+                // Sessions disabled
                 .sessionManagement().disable()
+                // CSRF disabled
                 .csrf().disable()
+                // Security for HTTP requests enabled
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()
-                );
+                            // All endpoints require authentication
+                            .anyRequest().authenticated()
+                )
+                // OAuth 2.0 resource server config
+                .oauth2ResourceServer(oauth2 ->{
+                    // Convert Jwt to AbstractAuthenticationToken
+                    JwtAuthenticationConverter authConverter = new JwtAuthenticationConverter();
 
+                    // Convert Jwt roles claim to GrantedAuthorities
+                    JwtGrantedAuthoritiesConverter roleConverter = new JwtGrantedAuthoritiesConverter();
+                    roleConverter.setAuthorityPrefix("ROLE_");
+                    roleConverter.setAuthoritiesClaimName("roles");
+
+                    // Jwt -> GrantedAuthorities -> AbstractAuthenticationToken
+                    authConverter.setJwtGrantedAuthoritiesConverter(roleConverter);
+
+                    // JWT authentication and AC from claims enabled
+                    oauth2.jwt().jwtAuthenticationConverter(authConverter);
+                });
         return http.build();
     }
 
--- src/main/java/com/thesis/api/controllers/BookController.java
+++ src/main/java/com/thesis/api/controllers/BookController.java
@@ -3,11 +3,13 @@ package com.thesis.api.controllers;
 import com.thesis.api.models.Book;
 import com.thesis.api.services.book.BookService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Collection;
 
 @RestController
@@ -20,12 +22,51 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    // GET: api/v1/book | return all books
+    // GET: api/v1/book | Return all books ---------------------------------------------------------------------------
     @GetMapping
     public ResponseEntity<?> findAllBooks(){
 
         Collection<Book> books = bookService.findAll();
 
         return ResponseEntity.ok(books);
+    }
+
+    // POST: api/v1/book | Add a new book ----------------------------------------------------------------------------
+    @PostMapping
+    @PreAuthorize("hasRole('user')")
+    public ResponseEntity<?> addBook(@AuthenticationPrincipal Jwt jwt){
+
+        final String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+        URI uri = URI.create(baseUrl+"/api/v1/book/"+1/*ID HERE*/);
+
+        return ResponseEntity.created(uri).body(jwt.getClaims());
+    }
+
+    // PUT: api/v1/book/{book_id} | Update entire existing book object -----------------------------------------------
+    @PutMapping(path = "/{book_id}")
+    public ResponseEntity<?> updateBook(
+            @PathVariable int book_id
+    ){
+
+        return ResponseEntity.noContent().build();
+    }
+
+    // PATCH: api/v1/book/{book_id} | Update existing book partially -------------------------------------------------
+    @PatchMapping
+    public ResponseEntity<?> updateBookPartial(
+            @PathVariable int book_id
+    ){
+
+        return ResponseEntity.noContent().build();
+    }
+
+    // DELETE: api/v1/book/{book_id} | Delete a book by its id -------------------------------------------------------
+    @DeleteMapping(path = "/{book_id}")
+    @PreAuthorize("hasRole('admin')")
+    public ResponseEntity<?> deleteBook(
+            @PathVariable int book_id
+    ){
+
+        return ResponseEntity.noContent().build();
     }
 }
