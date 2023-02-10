dbef1c551d62ad7545384bded95c93296e81b902
unknown <jani45@gmail.com>
2023-02-10 11:43:28 +0200

    mock data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO "book" (isbn, publish_date, title) VALUES
+('978-92-95055-02-5', '13-12-2022', 'Meaning of Life'),
+('888-92-95017-03-5', '01-02-2009', 'Book Two'),
+('654-92-95111-03-9', '11-08-2017', 'World in Ruins');
